9aaa5a5527651500ce500cc0bc6096b680c840b1
nhan81 <125139950+nhan81@users.noreply.github.com>
2023-03-08 21:46:50 +0700

    Add files via upload

---
+++ BuiNguyenThanhNhan/LAB04/SinhVienIT.java
@@ -0,0 +1,23 @@
+
+package LAB04;
+public class SinhVienIT extends SinhVien
+{
+    public double diemJava;
+    public double diemCss;
+    public double diemHTML;
+    
+    public SinhVienIT(String name,String nganh,double diemJava, double diemCss, double diemHTML) 
+    {
+        super (name,nganh);
+        this.diemJava = diemJava;
+        this.diemCss = diemCss;
+        this.diemHTML = diemHTML;
+    }
+
+    @Override
+    double getdiem()
+    {
+    return (2*diemJava+diemHTML+diemCss)/4;
+    }
+
+}
---
+++ BuiNguyenThanhNhan/LAB04/test.java
@@ -0,0 +1,12 @@
+
+package LAB04;
+
+import LAB04.DanhSachSinhVien;
+public class test {
+    public static void main (String[] arg)
+    {
+    DanhSachSinhVien dssv = new DanhSachSinhVien();
+    dssv.menu();
+    
+    }
+}
